5c8f5a9c584af94dda6b5a05d4a6fd39f53e7b85
eskilsen <eskil_hognestad@hotmail.com>
2019-09-03 12:59:58 +0200

    some functionality

--- sddm/src/readWriteProgram.java
+++ sddm/src/readWriteProgram.java
@@ -12,14 +12,42 @@ public class readWriteProgram {
 
     private static final int BLOCKSIZE = 8192;
     private static final long NBLOCKS = 32 * 131072;
-
-
+    
     Path file = Paths.get(System.getProperty("user.dir"), "myjavadata");
     SeekableByteChannel out = Files.newByteChannel(file, EnumSet.of(CREATE, APPEND));
     ByteBuffer buff = ByteBuffer.allocate(BLOCKSIZE);
 
     public readWriteProgram() throws IOException {
         out.write(buff);
+    }
+    
+    public void writer() throws IOException{
+        long start = System.currentTimeMillis();
+        
+        
+        String randomSentence = "Hello motherfucker lets dance guns blazing\n";
+        for (int i = 0; i<BLOCKSIZE; i++){
+            Files.write(file,randomSentence.getBytes(), StandardOpenOption.APPEND);    
+        }
+        
+        
+        long end = System.currentTimeMillis();
+        System.out.println(end-start);
+    }
+
+    public Path getFile(){
+        return file;
+    }
+    
+    public static void main(String[] args) throws IOException {
+     
+        readWriteProgram r = new readWriteProgram();
+        System.out.println(r.buff);
+        System.out.println(r.out);
+        
+        r.writer();
+        
+        System.out.format("toString: %s%n", r.file.toString());
     }
 
 }
